41e1bdce21c9a89b1fb614b81e52b32808ef3474
Vyntexed <Vyntexed@proton.me>
2026-04-13 13:04:10 -0700

    Update src.lua

--- src.lua
+++ src.lua
@@ -7,55 +7,55 @@ local library = {}
 
 library.themes = {
 	dark = {
-		
+
 		Font = "Inter",
-		
+
 		bg_primary = {11, 15, 26,0.2},
 		bg_secondary = {24, 28, 45, 0.45},
 		bg_tertiary = {30, 35, 55, 0.55},
-		
+
 		-- buttons
-			
+
 		button_primary = {0, 122, 255, 0.3},
 		button_primary_text = {255,255,255,0},
-		
+
 		button_primary_hover = {0, 122, 255, 0.10},
 		button_primary_pressed = {0, 102, 215, 0.05},
 
 		button_primary_stroke = {255, 255, 255, 0.65},
 		button_primary_highlight = {255, 255, 255, 0.7},
-		
+
 		-- btn secondary
-		
+
 		button_secondary = {255, 255, 255, 0.82},
 		button_secondary_text = {255,255,255,0.08},
 		button_secondary_stroke = {255,255,255,0.8},
-		
+
 		button_secondary_deselected = {255, 255, 255, 1},
 		button_secondary_text_deselected = {255,255,255,0.55},
 		button_secondary_stroke_deselected = {255,255,255,1},
-		
-		
+
+
 		stroke_primary = {255, 255, 255, 0.88},
-		
+
 		-- text
 		text_primary = {255, 255, 255, 0},
 		text_secondary = {235, 235, 245, 0.4},
 		text_tertiary = {235, 235, 245, 0.7},
 		text_disabled = {235,235, 245, 0.82}
-		
+
 	},
-	
+
 	light = {
-		
+
 		Font = "Inter",
-		
+
 		bg_primary = {245, 247, 252,0.4},
 		bg_secondary = {245, 247, 252, 0.75},
 		bg_tertiary = {235, 238, 245, 0.85},
-		
+
 		-- buttons
-		
+
 		button_primary = {0, 122, 255, 0.25},
 		button_primary_hover = {0, 122, 255, 0.18},
 		button_primary_pressed = {0, 102, 215, 0.12},
@@ -63,19 +63,19 @@ library.themes = {
 
 		button_primary_stroke = {255, 255, 255, 0.4},
 		button_primary_highlight = {255, 255, 255, 0.7},
-		
+
 		-- btn secondary
-		
+
 		button_secondary = {0, 0, 0, 0.88},
 		button_secondary_text = {0, 0, 0, 0.08},
 		button_secondary_stroke = {0, 0, 0, 0.82},
 		button_secondary_deselected = {0, 0, 0, 1},
 		button_secondary_text_deselected = {0, 0, 0, 0.55},
 		button_secondary_stroke_deselected = {0, 0, 0, 1},
-			
+
 		stroke_primary = {60, 60, 67, 0.85},
-		
-		
+
+
 		-- text
 		text_primary = {0, 0, 0,0},
 		text_secondary = {60, 60, 67,0.4},
@@ -109,7 +109,7 @@ local logger = {
 	warn = function(name,content)
 		warn("Quartz: "..name.." - "..content)
 	end,
-	
+
 	error = function(name,content)
 		error("Quartz: "..name.." - "..content)
 	end,
@@ -146,10 +146,10 @@ function library:init(data)
 
 		if data.secure == true then
 			library._internal.settings.secure = data.secure
-			
+
 			xpcall(function()
 				library._internal.screen.Parent = game:GetService("CoreGui")
-				
+
 				if library._internal.screen.Parent == nil then
 					hasBeenParented = false
 				else
@@ -158,11 +158,11 @@ function library:init(data)
 			end, function()
 				hasBeenParented = false
 			end)
-			
+
 			if hasBeenParented == false then
 				logger.error("secure","Unable to access CoreGui, since secure is enabled execution has been stopped")
 			end
-			
+
 			misc.blur = function()
 				return {
 					Destroy = function() end,
@@ -171,12 +171,12 @@ function library:init(data)
 			end	
 		end
 	end
-	
+
 	if hasBeenParented == false then
 		hasBeenParented = true
 		library._internal.screen.Parent = localPlayer.PlayerGui
 	end
-	
+
 	repeat
 		task.wait(0.1)
 	until
@@ -479,15 +479,96 @@ local ElementCreator = {
 		a.BackgroundTransparency = 1
 		a.TextScaled = true
 		a.Parent = library._internal.new_parent
-		a.Size = misc.table_to_UDIM2({0.518, 0},{0.08, 0})
+		a.Size = misc.table_to_UDIM2({0.9, 0},{0.08, 0})
 		a.TextXAlignment = Enum.TextXAlignment.Left
 		a.Parent = library._internal.latestContainer
-		
+
 		a.FontFace = NewFont(Enum.FontWeight.Bold,Enum.FontStyle.Normal)
 
 		a.TextTransparency = misc.get_color(library.theme.text_primary)[2]
 		a.TextColor3 = misc.get_color(library.theme.text_primary)[1]
-		
+
+		return {
+			SetText = function(c)
+				a.Text = c
+			end,
+			GetText = function(c)
+				return a.Text
+			end,
+			structure = {
+				label = a
+			}
+		}
+	end,
+	
+	Heading = function(self, Text)
+		local a = misc.Label(Text)
+		a.BackgroundTransparency = 1
+		a.TextScaled = true
+		a.Parent = library._internal.new_parent
+		a.Size = misc.table_to_UDIM2({0.9, 0},{0.072, 0})
+		a.TextXAlignment = Enum.TextXAlignment.Left
+		a.Parent = library._internal.latestContainer
+
+		a.FontFace = NewFont(Enum.FontWeight.SemiBold,Enum.FontStyle.Normal)
+
+		a.TextTransparency = misc.get_color(library.theme.text_primary)[2]
+		a.TextColor3 = misc.get_color(library.theme.text_primary)[1]
+
+		return {
+			SetText = function(c)
+				a.Text = c
+			end,
+			GetText = function(c)
+				return a.Text
+			end,
+			structure = {
+				label = a
+			}
+		}
+	end,
+	
+	Subheading = function(self, Text)
+		local a = misc.Label(Text)
+		a.BackgroundTransparency = 1
+		a.TextScaled = true
+		a.Parent = library._internal.new_parent
+		a.Size = misc.table_to_UDIM2({0.9, 0},{0.068, 0})
+		a.TextXAlignment = Enum.TextXAlignment.Left
+		a.Parent = library._internal.latestContainer
+
+		a.FontFace = NewFont(Enum.FontWeight.SemiBold,Enum.FontStyle.Normal)
+
+		a.TextTransparency = misc.get_color(library.theme.text_primary)[2]
+		a.TextColor3 = misc.get_color(library.theme.text_primary)[1]
+
+		return {
+			SetText = function(c)
+				a.Text = c
+			end,
+			GetText = function(c)
+				return a.Text
+			end,
+			structure = {
+				label = a
+			}
+		}
+	end,
+	
+	Paragraph = function(self, Text)
+		local a = misc.Label(Text)
+		a.BackgroundTransparency = 1
+		a.TextScaled = true
+		a.Parent = library._internal.new_parent
+		a.Size = misc.table_to_UDIM2({0.9, 0},{0.06, 0})
+		a.TextXAlignment = Enum.TextXAlignment.Left
+		a.Parent = library._internal.latestContainer
+
+		a.FontFace = NewFont(Enum.FontWeight.SemiBold,Enum.FontStyle.Normal)
+
+		a.TextTransparency = misc.get_color(library.theme.text_primary)[2]
+		a.TextColor3 = misc.get_color(library.theme.text_primary)[1]
+
 		return {
 			SetText = function(c)
 				a.Text = c
@@ -503,13 +584,13 @@ local ElementCreator = {
 }
 
 function library:Window(data, ...)
-	
+
 	local args = {...}
-	
+
 	if data ~= nil then
 		table.insert(args,data)
 	end
-	
+
 
 	local CurrentStyle = {}
 
@@ -548,9 +629,9 @@ function library:Window(data, ...)
 		cache.flags[Flag] = false
 		return false
 	end
-	
+
 	local load_window = { t = tick(), w = nil, bl = nil}
-	
+
 	if data ~= nil then
 
 		if data.loadingWindow ~= nil and data.loadingWindow.Enabled == true then
@@ -617,64 +698,64 @@ function library:Window(data, ...)
 
 			versionlabel.Size = misc.table_to_UDIM2({0.576, 0},{0.114, 0})
 			versionlabel.Position = misc.table_to_UDIM2({0.378, 0},{0.795, 0})
-			
+
 			if hasFlag(library.Flags.NoAnimations) == false then
 				local vT = versionlabel.TextTransparency
 				local sT = Subtitle.TextTransparency
 				local tT = TitleLabel.TextTransparency
 				local wT = lW.BackgroundTransparency
 				local wdT = win_dec.BackgroundTransparency
-								
+
 				TitleLabel.TextTransparency = 1
 				versionlabel.TextTransparency = 1
 				Subtitle.TextTransparency = 1
 				win_dec.BackgroundTransparency = 1
 				lW.BackgroundTransparency = 1
-				
+
 				local t1 = tweenService:Create(versionlabel,TweenInfo.new(0.3,Enum.EasingStyle.Circular,Enum.EasingDirection.Out),{TextTransparency=vT})
 				local t2 = tweenService:Create(Subtitle,t1.TweenInfo,{TextTransparency=sT})
 				local t3 = tweenService:Create(TitleLabel,t1.TweenInfo,{TextTransparency=tT})
 
 				local t4 = tweenService:Create(lW,t1.TweenInfo,{BackgroundTransparency=wT})
 				local t5 = tweenService:Create(win_dec,t1.TweenInfo,{BackgroundTransparency=wdT})
-				
+
 				lW.Visible = true
 
 				t1:Play(); t2:Play(); t3:Play(); t4:Play(); t5:Play()
-				
+
 			end
-			
+
 			load_window.bl = misc.blur(lW)	
 			lW.Visible = true
 
 		end
 
 	end
-	
+
 	local window = misc.Frame()
 	window.Visible = false
 	window.Parent = library._internal.screen
-	
+
 	window.Size = misc.table_to_UDIM2({0.506, 0},{0.651, 0})
 	window.Position = UDim2.new(0.5,0,0.5,0)
 	window.AnchorPoint = Vector2.new(0.5,0.5)
 	window.ZIndex = -2
-	
+
 	misc.blur(window)
 	misc.UiCorner({0.036, 0})
 	local winAR = misc.UiAspectRatio(1.561)
 	winAR.Parent = window
-	
+
 	local ustr = misc.UiStroke(window)
 	ustr.Color = misc.get_color(library.theme.stroke_primary)[1]
 	ustr.Transparency = misc.get_color(library.theme.stroke_primary)[2]
 	ustr.Thickness = 2
-	
+
 	window.BackgroundTransparency = misc.get_color(library.theme.bg_primary)[2]
 	window.BackgroundColor3 = misc.get_color(library.theme.bg_primary)[1]
 	library._internal.latestWindow = window
 	library._internal.latestContainer = window
-	
+
 	local win_dec = misc.Frame()
 	win_dec.ZIndex = -1
 	win_dec.Parent = window
@@ -684,24 +765,24 @@ function library:Window(data, ...)
 	uigrad_dec.Rotation = -90
 	uigrad_dec.Color = ColorSequence.new({ColorSequenceKeypoint.new(0,Color3.fromRGB(255,255,255)),ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,255))})
 	uigrad_dec.Transparency = NumberSequence.new({NumberSequenceKeypoint.new(0,1),NumberSequenceKeypoint.new(1,0.75)})
-	
+
 	local topbar = misc.Frame()
 
 	topbar.ZIndex = 9999999
-	
+
 	topbar.Parent = window
 	topbar.Size = misc.table_to_UDIM2({1, 0},{0.071, 0})
 	topbar.Position = misc.table_to_UDIM2({0, 0},{0, 0})
 	topbar.BackgroundTransparency = 1
-	
+
 	local topbar_button_holder = misc.Frame()
-	
+
 
 	topbar_button_holder.ZIndex = 9999999
 	topbar_button_holder.Parent = topbar
 	topbar_button_holder.Size = misc.table_to_UDIM2({0.194, 0},{1, 0})
 	topbar_button_holder.BackgroundTransparency = 1
-	
+
 	local x_btn = misc.Button(topbar_button_holder)
 	x_btn.BackgroundColor3 = Color3.fromRGB(252, 82, 85)
 	x_btn.Text = ""
@@ -710,7 +791,7 @@ function library:Window(data, ...)
 	x_btn.Size = misc.table_to_UDIM2({0.5, 0},{0.5, 0})
 	misc.UiAspectRatio(1,x_btn)
 	misc.UiCorner({1,0},x_btn)
-	
+
 	local fullscreen_btn = misc.Button(topbar_button_holder)
 	fullscreen_btn.BackgroundColor3 = Color3.fromRGB(44, 191, 77)
 	fullscreen_btn.Text = ""
@@ -719,7 +800,7 @@ function library:Window(data, ...)
 	fullscreen_btn.ZIndex = 9999999
 	misc.UiAspectRatio(1,fullscreen_btn)
 	misc.UiCorner({1,0},fullscreen_btn)
-	
+
 	local minimize_btn = misc.Button(topbar_button_holder)
 	minimize_btn.BackgroundColor3 = Color3.fromRGB(245, 193, 7)
 	minimize_btn.LayoutOrder = 2
@@ -728,11 +809,11 @@ function library:Window(data, ...)
 	misc.UiAspectRatio(1,minimize_btn)
 	misc.UiCorner({1,0},minimize_btn)
 	minimize_btn.ZIndex = 9999999
-	
+
 	local padding = misc.UiPadding(topbar_button_holder)
 	padding.PaddingLeft = UDim.new(0.06, 0)
 	padding.PaddingTop = UDim.new(0.1, 0)
-	
+
 	local ULL = misc.UiListLayout(topbar_button_holder)
 	ULL.VerticalAlignment = Enum.VerticalAlignment.Center
 	ULL.Padding = UDim.new(0.06, 0)
@@ -740,13 +821,13 @@ function library:Window(data, ...)
 	ULL.SortOrder = Enum.SortOrder.LayoutOrder
 
 	if load_window.w ~= nil then
-		
+
 		local dur = data.loadingWindow.minLoadTime or  3
-		
+
 		if tick()-load_window.t <= dur then
 			task.wait(math.clamp( ( dur - (tick()-load_window.t) ),0,dur+5))
 		end
-		
+
 		if hasFlag(library.Flags.NoAnimations) == true then
 			load_window.w.Visible = false
 			window.Visible = true
@@ -756,14 +837,14 @@ function library:Window(data, ...)
 		else
 			local t1 = tweenService:Create(load_window.w,TweenInfo.new(0.3,Enum.EasingStyle.Circular,Enum.EasingDirection.Out),{Size=window.Size,Position=window.Position})
 			local t2 = tweenService:Create(load_window.s,t1.TweenInfo,{AspectRatio=winAR.AspectRatio})
-			
+
 			for i, v in pairs(load_window.w:GetDescendants()) do
 				if v:IsA("TextLabel") then
 					v.Transparency = 1
 					--tweenService:Create(v,TweenInfo.new(t1.TweenInfo.Time*1,Enum.EasingStyle.Linear,Enum.EasingDirection.Out),{TextTransparency=1}):Play()
 				end
 			end
-			
+
 			t1:Play()
 			t2:Play()
 			t1.Completed:Wait()
@@ -771,11 +852,11 @@ function library:Window(data, ...)
 			load_window.w.Visible = false
 			load_window.w:Destroy()
 		end
-		
+
 	end
-	
+
 	window.Visible = true
-	
+
 	local hooks = {
 		buttons = {
 			close_btn = {},
@@ -841,52 +922,52 @@ function library:Window(data, ...)
 			}
 		}
 	}
-	
+
 	if hasFlag(library.Style.Dashboard) then
 		local sidebar = misc.Frame()
-		
+
 		sidebar.BackgroundTransparency = misc.get_color(library.theme.bg_secondary)[2]
 		sidebar.BackgroundColor3 = misc.get_color(library.theme.bg_secondary)[1]
-		
+
 		sidebar.Parent = window
 		sidebar.Size = misc.table_to_UDIM2({0.294, 0},{1.002, 0})
 		sidebar.Position = misc.table_to_UDIM2({0, 0},{0, 0})
-		
+
 		local gradUi = misc.UiGradient(sidebar)
 		gradUi.Color = ColorSequence.new({ColorSequenceKeypoint.new(0,Color3.fromRGB(255,255,255)),ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,255))})
 		gradUi.Transparency = NumberSequence.new({NumberSequenceKeypoint.new(0,0.381),NumberSequenceKeypoint.new(1,0.681)})
-		
+
 		misc.UiCorner({0.1,0},sidebar)
-		
+
 		misc.UiAspectRatio(0.458,sidebar)
-		
+
 		misc.UiPadding(sidebar).PaddingTop = UDim.new(0.1,0)
-		
+
 		local lsa = misc.UiListLayout(sidebar)
 		lsa.HorizontalAlignment = Enum.HorizontalAlignment.Center
 		lsa.Padding = UDim.new(0.05,0)
-		
+
 		library._internal.style = {}
 		library._internal.style.sidebar = sidebar
 		library._internal.style.sidebar_sel = nil
-		
+
 		library._internal.style.tabHolder = misc.Frame()
 		library._internal.style.tabHolder.Parent = window
 		library._internal.style.tabHolder.Size = UDim2.new(1-sidebar.Size.X.Scale,0,1,0)
 		library._internal.style.tabHolder.Position = UDim2.new(sidebar.Size.X.Scale,0,0,0)
 		library._internal.style.tabHolder.BackgroundTransparency = 1
 	end
-	
+
 	to_return.Button = function(self,Text,Variant)
 		local btn = misc.Button()
-		
+
 		Variant = Variant or library.Components.Button.Primary
-		
+
 		btn.Text = Text or "Button"
-		
+
 		local rtrn = {}
 		rtrn.structure = {btn=btn}
-		
+
 		if Variant == library.Components.Button.Primary then
 			btn.BackgroundColor3 = misc.get_color(library.theme.button_primary)[1]
 			btn.BackgroundTransparency = misc.get_color(library.theme.button_primary)[2]
@@ -894,17 +975,17 @@ function library:Window(data, ...)
 
 			btn.TextTransparency = misc.get_color(library.theme.button_primary_text)[2]
 			btn.TextColor3 = misc.get_color(library.theme.button_primary_text)[1]
-			
+
 			local grad1 = misc.UiGradient(btn)
 			grad1.Color = ColorSequence.new({ColorSequenceKeypoint.new(0,Color3.fromRGB(255,255,255)),ColorSequenceKeypoint.new(1,Color3.fromRGB(255,255,255))})
 			grad1.Transparency = NumberSequence.new({NumberSequenceKeypoint.new(0,0),NumberSequenceKeypoint.new(1,0)})
-			
+
 			local str = misc.UiStroke(btn)
 			str.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 			str.Color = misc.get_color(library.theme.button_primary_stroke)[1]
 			str.Transparency = misc.get_color(library.theme.button_primary_stroke)[2]
 			str.Thickness = 1
-			
+
 			local fr = Instance.new("Frame",btn)
 			fr.BackgroundColor3 = Color3.fromRGB(255,255,255)
 			fr.Size = misc.table_to_UDIM2({1,0},{1,0})
@@ -912,7 +993,7 @@ function library:Window(data, ...)
 
 			local uicorn = misc.UiCorner({0.25,0},btn)
 			uicorn:Clone().Parent = fr
-			
+
 			local cak = misc.UiGradient(fr)
 			cak.Color = ColorSequence.new(
 				{
@@ -920,9 +1001,9 @@ function library:Window(data, ...)
 					ColorSequenceKeypoint.new(1,misc.get_color(library.theme.button_primary_highlight)[1])})
 			cak.Rotation = 90
 			cak.Transparency = NumberSequence.new({NumberSequenceKeypoint.new(0,misc.get_color(library.theme.button_primary_highlight)[2]),NumberSequenceKeypoint.new(1,1)})
-			
+
 			-- return
-			
+
 			rtrn.structure.stroke = str
 			rtrn.structure.outerGradient = grad1
 			rtrn.structure.innerFrame = fr
@@ -933,42 +1014,42 @@ function library:Window(data, ...)
 			btn.TextTransparency = misc.get_color(library.theme.button_secondary_text)[2]
 			btn.TextColor3 = misc.get_color(library.theme.button_secondary_text)[1]
 			btn.TextScaled = true
-			
+
 			misc.UiCorner({0.25, 0},btn)
-			
+
 			local str = misc.UiStroke(btn)
 			str.Color = misc.get_color(library.theme.button_secondary_stroke)[1]
 			str.Transparency = misc.get_color(library.theme.button_secondary_stroke)[2]
 			str.Thickness = 1
 			str.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-			
+
 			-- return	
 			rtrn.structure.stroke = str
-			
+
 		else
 			logger.error("Components","Unknown component variant "..tostring(Variant))
 			return rtrn
 		end
-		
+
 		return rtrn
 	end
 
 	to_return.Tab = function(self,Title)
 		local rtrn = {}
-		
+
 		local parentingT = nil
-		
+
 		if hasFlag(library.Style.Custom) == true then
 			logger.error("styles","Using custom style, no tab could be created")
 			return
 		elseif hasFlag(library.Style.Dashboard) then
 			local sidebar = library._internal.style.sidebar
 			local tabHolder = library._internal.style.tabHolder
-			
+
 			local a = to_return:Button(Title,library.Components.Button.Secondary)
 			a.structure.btn.Parent = sidebar
 			a.structure.btn.Size = misc.table_to_UDIM2({0.787, 0},{0.068, 0})
-			
+
 			parentingT = misc.Frame()
 			misc.UiListLayout(parentingT).Padding = UDim.new(0.025,0)
 			local cab = misc.UiPadding(parentingT)
@@ -978,7 +1059,7 @@ function library:Window(data, ...)
 			parentingT.Position = UDim2.new(0,0,0,0)
 			parentingT.Parent = tabHolder
 			parentingT.BackgroundTransparency = 1
-			
+
 			if library._internal.style.sidebar_sel == nil then
 				library._internal.style.sidebar_sel = a.structure.btn
 			else
@@ -993,15 +1074,15 @@ function library:Window(data, ...)
 				a.structure.stroke.Transparency = misc.get_color(library.theme.button_secondary_stroke_deselected)[2]
 			end
 		end
-		
+
 		if parentingT ~= nil then
 			library._internal.latestContainer = parentingT
 			library._internal.latestTab = parentingT
 		end
-		
+
 		return ElementCreator
 	end
-	
+
 	if hasFlag(library.Flags.NoDrag) == false then
 		misc.add_drag(topbar,window)
 	end
@@ -1032,7 +1113,7 @@ function library:Window(data, ...)
 		--	end	
 		--end)
 	end
-	
+
 	return to_return
 end
 
